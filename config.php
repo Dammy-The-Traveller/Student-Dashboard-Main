efe87f898c35a2b3b2c9244bde64c95cac646e86
DAMILARE DAVID ADEBESIN <162614611+Dammy-The-Traveller@users.noreply.github.com>
2025-01-03 05:13:23 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 //echo "hello there";
 return  [
   'database' => [
-    'host' => 'db', // Database server host
+    'host' => 'localhost', // Database server host
     'port' => 3306, // Database server port
     'dbname'=> 'lemsas', // Database name
     'charset' => 'utf8mb4' // Character set for encoding
